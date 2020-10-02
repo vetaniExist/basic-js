a2cafdd94a3218a2386ee9bf38ecb7d9b124f61a
Huriosity <vetani3123@mail.ru>
2020-10-02 13:04:30 +0300

    feat: complete dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let annagramma = [];
+  for(member of members) {
+    if (typeof member === "string") {
+      member = member.replace(/ /g, "");
+      annagramma.push(member[0].toUpperCase());
+    }
+  }
+
+
+  return annagramma.sort().join('');
 };
