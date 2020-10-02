3f2d51cc2a4267f24eeb0d04d305cc026d34aa82
Huriosity <vetani3123@mail.ru>
2020-10-02 12:48:12 +0300

    feat: implement curbon-dating method

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,24 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  //numActivity это то что нам передается в функцию на вход, но преобразованное из строки в число? Т.е. это и есть активность изотопа в образце по форммуле?
+
+  // k = 0.693/HALF_LIFE_PERIOD;    t = Math.log2(MODERN_ACTIVITY/numActivity) / k;
+  let iSampleActivity = parseInt(sampleActivity);/// A
+
+  if ( Number.isNaN(iSampleActivity) || iSampleActivity < 0) {
+    return false;
+  }
+
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  let time = Math.log(MODERN_ACTIVITY/iSampleActivity) / k
+
+  if (time > 9000 || time <= 0) {
+    return false;
+  }
+
+  return time;
+
+
 };
