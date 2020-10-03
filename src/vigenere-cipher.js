c789332e560ce8d3a7859e32c29ba2a522d25e85
Huriosity <vetani3123@mail.ru>
2020-10-03 20:59:26 +0300

    feat: complete vigenere cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,8 +3,7 @@ const CustomError = require("../extensions/custom-error");
 class VigenereCipheringMachine {
 
   constructor(bIsDirect) {
-
-
+    this.bIsDirect =bIsDirect
   }
 
   encrypt(message, key) {
@@ -16,7 +15,6 @@ class VigenereCipheringMachine {
     let calibratedMessage = message.replace(/ /g, "");
     let calibratedKey = configurateCalibratedKeyRow(calibratedMessage,key);
 
-    
     let result = "";
     
     for( let i = 0; i < calibratedMessage.length; i += 1) {
@@ -24,13 +22,13 @@ class VigenereCipheringMachine {
       let keyCodeShift = isUpperCase(calibratedKey[i]) ?  65 : 97;
  
 
-      let messageShift = calibratedMessage.charCodeAt(i);
+      let messageShift = calibratedMessage.charCodeAt(i);// m
       if (messageShift < codeShift || messageShift > codeShift + 26) {
         result += calibratedMessage[i];
         continue;
       }
       messageShift -= codeShift;
-      let keyShift = calibratedKey.charCodeAt(i) - keyCodeShift;
+      let keyShift = calibratedKey.charCodeAt(i) - keyCodeShift; // k
       let shiftedCharCode = (keyShift + messageShift ) % 26 + keyCodeShift;
 
 
@@ -43,16 +41,45 @@ class VigenereCipheringMachine {
         result = result.slice(0,i) + " " + result.slice(i);
       }
     }
-    for (let i = 0; i < missingSpaces; i += 1) {
-   
+    if (this.bIsDirect === false) {
+      return result.split('').reverse().join('').toUpperCase();
+    }
+    return result.toUpperCase();
+  }
+
+  decrypt(message, key) {
+    if (!message || ! key) {
+      throw new Error();
     }
 
-    return result.toUpperCase();
-    
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let cryptedMessage = message.replace(/ /g, "");
+    let calibratedKey = configurateCalibratedKeyRow(cryptedMessage,key);
+
+    let result = "";
+
+    for( let i = 0; i < cryptedMessage.length; i += 1) {
+      let messageShift = cryptedMessage.charCodeAt(i);// c
+      if (messageShift < 65 || messageShift > 65 + 26) {
+        result += cryptedMessage[i];
+        continue;
+      }
+      messageShift -= 65;
+      let keyCodeShift = isUpperCase(calibratedKey[i]) ?  65 : 97;
+      let keyShift = calibratedKey.charCodeAt(i) - keyCodeShift; // k
+      let shiftedCharCode = (messageShift + 26 - keyShift ) % 26 + 65;
+
+      result += String.fromCharCode(shiftedCharCode);
+    }
+
+    for (let i = 0; i < message.length; i += 1) {
+      if (message[i] === " ") {
+        result = result.slice(0,i) + " " + result.slice(i);
+      }
+    }
+    if (this.bIsDirect === false) {
+      return result.split('').reverse().join('');
+    }
+    return result;
   }
 }
 
